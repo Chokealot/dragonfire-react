feec8c3bb702bb612ac056d941ab3dfc9990d5db
Casper Konyi <casper.konyi@cordel.no>
2022-10-03 16:19:54 +0200

    AppBar added

--- dragonfire-react/src/App.tsx
+++ dragonfire-react/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menubar from './components/Menubar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Menubar />
     </div>
   );
 }
