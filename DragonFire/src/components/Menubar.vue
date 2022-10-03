9ad19db33c9eae07b0aad0a76bfde60e822492e6
Casper Konyi <casper.konyi@cordel.no>
2022-10-03 15:40:17 +0200

    yay

---
+++ DragonFire/src/components/Menubar.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import { ref } from 'vue';
+
+const open: any | undefined = ref(false)
+
+const handleClick: any = () => {
+    open.value = true
+    return console.log("Du har tryckt")
+}
+
+
+
+</script>
+
+<template> 
+    <button v-on:click=handleClick >TEST</button>
+    <Dialog :visible=open >Massa skit som ska stå här</Dialog>
+</template>
+
+
+
+<style>
+
+    
+
+</style>
