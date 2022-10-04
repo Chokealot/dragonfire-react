67c0e22b701c18fbf74dac078bf33b4812f103fc
Stian Natland <stian.natland@cordel.no>
2022-10-04 14:06:12 +0200

    Added achievements sorting

--- dragonfire-react/src/views/Achievements.tsx
+++ dragonfire-react/src/views/Achievements.tsx
@@ -1,8 +1,10 @@
-import { Avatar, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, TextField, Typography } from "@mui/material";
 import WorkIcon from '@mui/icons-material/Work';
+import React from "react";
 
 const Achievements = () => {
-    const achievementsList = [
+
+    const bruk = [
         {
             category: 'Bruk',
             name: 'Førstegangs innlogging',
@@ -24,6 +26,9 @@ const Achievements = () => {
             isComplete: false,
             points: 200,
         },
+    ];
+
+    const sjekklister = [
         {
             category: 'Sjekklister',
             name: 'Opprette sjekkliste',
@@ -52,48 +57,24 @@ const Achievements = () => {
             isComplete: false,
             points: 20,
         },
+    ]
+
+    const søknader = [
         {
-            category: 'Søknader',
-            name: 'Legg til søknad',
-            id: 8,
-            isComplete: false,
-            points: 50,
-        },
-        {
-            category: 'Søknader',
-            name: 'Sende inn søknad',
-            id: 9,
-            isComplete: false,
-            points: 100,
-        },
-        {
-            category: 'Timeregistrering',
-            name: 'Registrere samme dag',
-            id: 10,
-            isComplete: false,
-            points: 50,
-        },
-        {
-            category: 'Timeregistrering',
-            name: 'Registerer samme dag, 5 dager på rad',
-            id: 11,
-            isComplete: false,
-            points: 100,
-        },
-        {
-            category: 'Håndbok/prosedyrer',
-            name: 'Opprettet',
-            id: 12,
-            isComplete: false,
-            points: 100,
-        },
-        {
-            category: 'Håndbok/prosedyrer',
-            name: 'Lest',
-            id: 13,
-            isComplete: false,
-            points: 50,
-        },
+        category: 'Søknader',
+        name: 'Legg til søknad',
+        id: 8,
+        isComplete: false,
+        points: 50,
+    },
+    {
+        category: 'Søknader',
+        name: 'Sende inn søknad',
+        id: 9,
+        isComplete: false,
+        points: 100,
+    },];
+    const prosjekt = [
         {
             category: 'Prosjekt',
             name: 'Lagt inn full adresse',
@@ -122,6 +103,8 @@ const Achievements = () => {
             isComplete: false,
             points: 30,
         },
+    ];
+    const produktregister = [
         {
             category: 'Produktregister',
             name: 'Importer nye priser',
@@ -129,33 +112,121 @@ const Achievements = () => {
             isComplete: false,
             points: 150,
         },
-    ]
+    ];
+    const prosedyrer = [
+        {
+            category: 'Håndbok/prosedyrer',
+            name: 'Opprettet',
+            id: 12,
+            isComplete: false,
+            points: 100,
+        },
+        {
+            category: 'Håndbok/prosedyrer',
+            name: 'Lest',
+            id: 13,
+            isComplete: false,
+            points: 50,
+        },
+    ];
+    const timeregistrering = [
+        {
+            category: 'Timeregistrering',
+            name: 'Registrere samme dag',
+            id: 10,
+            isComplete: false,
+            points: 50,
+        },
+        {
+            category: 'Timeregistrering',
+            name: 'Registerer samme dag, 5 dager på rad',
+            id: 11,
+            isComplete: false,
+            points: 100,
+        },
+    ];
     return (
         <>
-            <h1>
-                Achievements view
-            </h1>
-            <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+            <h1>Achievements</h1>
+            <Typography sx={{textDecoration: "underline"}} alignItems="flex-start" display={"flex"} pl={1} variant="h4" component="h2">Bruk</Typography>
+            <List sx={{ width: '100%', bgcolor: 'background.paper', paddingBottom: 8 }}>
                 {
-                    achievementsList.map((achievement: any) => (
-                            <ListItem key={achievement.id}>
-                                <ListItemAvatar>
-                                    <Avatar sx={{ backgroundColor: achievement?.isComplete ? "#FFD700" : undefined}}>
-                                        <WorkIcon />
-                                    </Avatar>
-                                </ListItemAvatar>
-                            <ListItemText
-                                primary={achievement.name}
-                                primaryTypographyProps={!achievement?.isComplete ? {color: "lightgrey"} : {fontWeight: "bold"}}
-                                secondary="Jan 9, 2014"
-                                secondaryTypographyProps={!achievement?.isComplete ? {color: "lightgrey"} : {}}
-                            />
-                        </ListItem>
+                    bruk.map((achievements: any, index: number) => (
+                        createAchievementList(achievements)
+                    ))
+                }
+            </List>
+
+            <Typography sx={{textDecoration: "underline"}} alignItems="flex-start" display={"flex"} pl={1} variant="h4" component="h2">Sjekklister</Typography>
+            <List sx={{ width: '100%', bgcolor: 'background.paper', paddingBottom: 8 }}>
+                {
+                    sjekklister.map((achievements: any, index: number) => (
+                        createAchievementList(achievements)
+                    ))
+                }
+            </List>
+
+            <Typography sx={{textDecoration: "underline"}} alignItems="flex-start" display={"flex"} pl={1} variant="h4" component="h2">Søknader</Typography>
+            <List sx={{ width: '100%', bgcolor: 'background.paper', paddingBottom: 8 }}>
+                {
+                    søknader.map((achievements: any, index: number) => (
+                        createAchievementList(achievements)
+                    ))
+                }
+            </List>
+
+            <Typography sx={{textDecoration: "underline"}} alignItems="flex-start" display={"flex"} pl={1} variant="h4" component="h2">Prosjekt</Typography>
+            <List sx={{ width: '100%', bgcolor: 'background.paper', paddingBottom: 8 }}>
+                {
+                    prosjekt.map((achievements: any, index: number) => (
+                        createAchievementList(achievements)
+                    ))
+                }
+            </List>
+
+            <Typography sx={{textDecoration: "underline"}} alignItems="flex-start" display={"flex"} pl={1} variant="h4" component="h2">Produktregister</Typography>
+            <List sx={{ width: '100%', bgcolor: 'background.paper', paddingBottom: 8 }}>
+                {
+                    produktregister.map((achievements: any, index: number) => (
+                        createAchievementList(achievements)
+                    ))
+                }
+            </List>
+
+            <Typography sx={{textDecoration: "underline"}} alignItems="flex-start" display={"flex"} pl={1} variant="h4" component="h2">Håndbok/prosedyrer</Typography>
+            <List sx={{ width: '100%', bgcolor: 'background.paper', paddingBottom: 8 }}>
+                {
+                    prosedyrer.map((achievements: any, index: number) => (
+                        createAchievementList(achievements)
+                    ))
+                }
+            </List>
+
+            <Typography sx={{textDecoration: "underline"}} alignItems="flex-start" display={"flex"} pl={1} variant="h4" component="h2">Timeregistrering</Typography>
+            <List sx={{ width: '100%', bgcolor: 'background.paper', paddingBottom: 8 }}>
+                {
+                    timeregistrering.map((achievements: any, index: number) => (
+                        createAchievementList(achievements)
                     ))
                 }
             </List>
         </>
     )
+
+    function createAchievementList(achievement: any): JSX.Element {
+        return <ListItem key={achievement.id}>
+            <ListItemAvatar>
+                <Avatar sx={{ backgroundColor: achievement?.isComplete ? "#FFD700" : undefined }}>
+                    <WorkIcon />
+                </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+                primary={achievement.name}
+                primaryTypographyProps={!achievement?.isComplete ? { color: "lightgrey" } : { fontWeight: "bold" }}
+                secondary="Jan 9, 2014"
+                secondaryTypographyProps={!achievement?.isComplete ? { color: "lightgrey" } : {}} />
+        </ListItem>;
+    }
 };
 
 export default Achievements;
