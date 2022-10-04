1209ee48ece5762539b85259dbd8088992e86467
Stian Natland <stian.natland@cordel.no>
2022-10-04 12:45:10 +0200

    added log

--- dragonfire-react/src/App.tsx
+++ dragonfire-react/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Menubar from './components/Menubar';
 import AchivementSnackbar from './components/AchivementSnackbar';
-import { lazy, Suspense, useState } from 'react';
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ACHIEVEMENTSROUTE, APPLICATIONSROUTE, CHECKLISTSROUTE } from './routes/AppRoutes';
 import React from 'react';
@@ -22,6 +22,7 @@ function App() {
   const [open, setOpen] = React.useState(false);
   const profileMenuItems = ['Profile', 'Account', 'Dashboard', 'Logout'];
   const [ achivement, setAchivement ] = React.useState<any | undefined>("Test Achivement")
+  console.log("setAchivement", setAchivement);
 
   const handleAchivement = () => {
     if (open === true) {
